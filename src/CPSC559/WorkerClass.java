c3120568b18a1a9ca8e59bc260756cef4dcdb0ff
Zilin Ye <zilin.ye1@ucalgary.ca>
2020-02-27 15:24:49 -0700

    Handle the connection exception

--- src/CPSC559/WorkerClass.java
+++ src/CPSC559/WorkerClass.java
@@ -1,9 +1,9 @@
 package CPSC559;
-import java.io.IOException;
-import java.lang.Object;
+
 import java.net.Socket;
 import java.io.InputStream;
-import java.util.Arrays;
+import java.io.IOException;
+import java.net.ConnectException;
 
 public class WorkerClass {
     public static void main(String[]args) throws IOException {
@@ -13,18 +13,21 @@ public class WorkerClass {
         System.out.println("hello java welcome back");
         //public Socket(InetAddress address,
         //      int port)
-        Socket connect = new Socket("localhost",9000);
-        
-        InputStream input = connect.getInputStream();
-        
-        char nextChar = (char) input.read();
-        
-        while(nextChar != '\0') {
-        	System.out.print( nextChar );
-        	nextChar = (char) input.read();
-        }
-        
-        input.close();
+        try{
+            Socket connect = new Socket("localhost",9000);
+            InputStream input = connect.getInputStream();
+            char nextChar = (char) input.read();
 
+            while(nextChar != '\0') {
+                System.out.print( nextChar );
+                nextChar = (char) input.read();
+            }
+
+            input.close();
+        }catch (ConnectException ce){
+            System.out.println("Can not establish the connection");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
