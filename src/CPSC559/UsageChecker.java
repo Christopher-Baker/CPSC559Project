99925c05cd3aea0179aefd31fc450c60d32e572e
ErickPower <epower1996@gmail.com>
2020-04-07 16:53:27 -0600

    leader election is now modified bully. Changed from finding quietest server.

--- src/CPSC559/UsageChecker.java
+++ src/CPSC559/UsageChecker.java
@@ -117,12 +117,12 @@ public class UsageChecker implements Runnable {
 	}
 
 	public static synchronized int leaderElection() {
-		int minUsage = 9999;
 		int newPort = -1;
 		for(int i = 0; i < socketUsage.size(); ++i) {
-			if(socketUsage.get(i).usage() < minUsage && socketUsage.get(i).usage() > 0){
-				minUsage = socketUsage.get(i).usage();
+			//Get lowest ID socket, which is first alive socket.
+			if(socketUsage.get(i).usage() >= 0){
 				newPort = socketUsage.get(i).portNum();
+				return newPort;
 			}
 		}
 		
